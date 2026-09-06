f179c98206df8c3592c3342bb40fcff66c950c65
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:31:48 +0530

    Update Employee.java

--- Demo/Employee.java
+++ Demo/Employee.java
@@ -1,1 +1,10 @@
-
+package Demo;
+public class Employee {
+    protected double salary;
+    public Employee(double sal) {
+        salary = sal;
+    }
+    protected void displaySalary() {
+        System.out.println("Salary: " + salary);
+    }
+}
