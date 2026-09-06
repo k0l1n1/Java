487ffea0e7ee9859c8869647ed32f6148f60e004
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:39:39 +0530

    Add Prime number finder in Prime.java

---
+++ Lab1/Prime.java
@@ -0,0 +1,20 @@
+package Lab1;
+import java.util.Scanner;
+public class Prime {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter the number: ");
+	    int n = sc.nextInt();
+        for (int i = 2; i <= n; i++) {
+            boolean prime = true;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime)
+                System.out.print(i + " ");
+        }
+    }
+}
