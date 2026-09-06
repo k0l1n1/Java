b5933487cb644f0185952ed2edc5712ee351c505
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:57:39 +0530

    Add CarDemo class to demonstrate Car functionality

---
+++ PracticeQuestion6/CarDemo.java
@@ -0,0 +1,8 @@
+package PracticeQuestion6;
+public class CarDemo {
+    public static void main(String[] args) {
+        Car obj = new Car("Toyota");
+        //System.out.println(obj.model);
+        obj.showModel();
+    }
+}
