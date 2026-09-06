21ac9bb01b64161547364f100d0ece7c869279e9
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:35:57 +0530

    Implement factorial calculation in FactDemo

--- Lab1/FactDemo.java
+++ Lab1/FactDemo.java
@@ -1,1 +1,15 @@
-
+package Lab1;
+import java.util.Scanner;
+public class FactDemo {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        int fact = 1,n=x;
+        while (n > 0) {
+            fact = fact * n;
+            n--;
+        }
+        System.out.println("Factorial of "+ x + " is = "+ fact);
+    }
+}
