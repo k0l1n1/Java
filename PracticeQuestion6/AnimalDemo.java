a78c23a4c65464edfc66fc68bec41357eeda3bd9
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:56:31 +0530

    Add AnimalDemo class to demonstrate Animal usage

---
+++ PracticeQuestion6/AnimalDemo.java
@@ -0,0 +1,7 @@
+package PracticeQuestion6;
+public class AnimalDemo {
+    public static void main(String[] args) {
+        Animal obj = new Animal("Lion");
+        System.out.println("Species: " + obj.species);
+    }
+}
