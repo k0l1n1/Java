e24b324cef80406117fd9410d671a24c5c58b63f
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:37:18 +0530

    Implement GCD calculation in GcdDemo.java

--- Lab1/GcdDemo.java
+++ Lab1/GcdDemo.java
@@ -1,1 +1,17 @@
-
+package Lab1;
+import java.util.Scanner;
+public class GcdDemo {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("GCD = " + a);
+    }
+}
