3e62b33d27231de2808b7a33b96d93790c02b614
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:54:52 +0530

    Add VectorSum class to calculate sum of integers

---
+++ PracticeQuestion5/VectorSum.java
@@ -0,0 +1,17 @@
+package PracticeQuestion5;
+import java.util.Vector;
+public class VectorSum {
+    public static void main(String[] args) {
+        Vector<Integer> numbers = new Vector<Integer>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+        int sum = 0;
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
