18e8bed5c922b3b5e889093f44619e3e81bb07a9
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:56:58 +0530

    Create Car.java

---
+++ PracticeQuestion6/Car.java
@@ -0,0 +1,10 @@
+package PracticeQuestion6;
+class Car {
+    private String model;
+    Car(String m) {
+        model = m;
+    }
+    void showModel() {
+        System.out.println("Model: " + model);
+    }
+}
