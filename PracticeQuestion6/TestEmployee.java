d30c2cf95f0288537b1c1925be9400ba82f1c2db
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:58:34 +0530

    Add TestEmployee class for Employee testing

---
+++ PracticeQuestion6/TestEmployee.java
@@ -0,0 +1,9 @@
+package PracticeQuestion6;
+import Demo.Employee;
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee obj = new Employee(30000);
+        // System.out.println(obj.salary);
+        // obj.displaySalary();
+    }
+}
