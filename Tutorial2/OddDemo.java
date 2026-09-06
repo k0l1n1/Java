91bceebf85931e0afae68a74906155fa4ac23391
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 22:06:40 +0530

    Create OddDemo.java

---
+++ Tutorial2/OddDemo.java
@@ -0,0 +1,15 @@
+package Tutorial2; 
+import java.util.Scanner;
+public class OddDemo{
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter n: ");
+	        int n = sc.nextInt();
+	        System.out.println("Odd Numbers:");
+	        for (int i = 1; i <= n; i++) {
+	            if (i % 2 == 0)
+	                continue;
+	            System.out.print(i + " ");
+	        }
+	    }
+}
