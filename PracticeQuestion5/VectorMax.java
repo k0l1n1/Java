d676ff09a5c1a4d03767bd584e980b8b0c5771db
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:53:18 +0530

    Add VectorMax class to find maximum in a Vector

---
+++ PracticeQuestion5/VectorMax.java
@@ -0,0 +1,19 @@
+package PracticeQuestion5;
+import java.util.Vector;
+public class VectorMax {
+    public static void main(String[] args) {
+        Vector<Integer> numbers = new Vector<Integer>();
+        numbers.add(25);
+        numbers.add(60);
+        numbers.add(15);
+        numbers.add(80);
+        numbers.add(45);
+        int max = numbers.get(0);
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        System.out.println("Maximum = " + max);
+    }
+}
