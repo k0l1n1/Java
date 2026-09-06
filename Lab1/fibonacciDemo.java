e182f3d1ac839ad4dcf1891ccbab12ad1e9248d4
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:38:20 +0530

    Implement Fibonacci sequence generator

--- Lab1/fibonacciDemo.java
+++ Lab1/fibonacciDemo.java
@@ -1,1 +1,16 @@
-
+package Lab1;
+import java.util.Scanner;
+public class FibonacciDemo {
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the limit: ");
+    int n = sc.nextInt();
+    int a = 0, b = 1;
+    while (a <= n) {
+        System.out.print(a + " ");
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+	}
+}
