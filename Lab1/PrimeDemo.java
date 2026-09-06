7a99d021fae00c29dea88baf927cce4fa4aeb0de
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:40:34 +0530

    Create PrimeDemo.java

---
+++ Lab1/PrimeDemo.java
@@ -0,0 +1,24 @@
+package Lab1;
+import java.util.Scanner;
+public class PrimeDemo {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        boolean prime = true;
+        if (n <= 1) {
+            prime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        if (prime)
+            System.out.println(n + " is Prime");
+        else
+            System.out.println(n + " is Not Prime");
+    }
+}
