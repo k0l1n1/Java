9b63eb6e5bfcec0011b67f9d7e4b9e4a729c86cf
Kolin Biju <kolinbijukulangara@gmail.com>
2026-09-06 21:35:10 +0530

    Update EvenDemo.java

--- Lab1/EvenDemo.java
+++ Lab1/EvenDemo.java
@@ -1,1 +1,15 @@
+package Lab1;
+import java.util.Scanner;
+public class EvenDemo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = 2,n;
+		System.out.print("Enter a number: ");
+		n = sc.nextInt();
+	        do {
+	            System.out.print(i + " ");
+	            i += 2;
+	        } while (i <= n);
 
+	    }
+	}
